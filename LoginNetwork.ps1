1377f9eec8f987aa286f1ab0bf4f51eb36cc0f57
zjsxply <1609566137@qq.com>
2023-04-02 13:44:49 +0800

    fix issues, add a screenshot

--- LoginNetwork.ps1
+++ LoginNetwork.ps1
@@ -10,8 +10,15 @@ function Login-CampusNetwork {
     # 检测 IP 10.2.4.2 是否可以 Ping 通
     if (-not $Test) {
 		$pingResult = Test-Connection -ComputerName "10.2.4.2" -Quiet -BufferSize 1 -Count 1
+		if ($pingResult) {
+			$pingResultText = "网络处于连通状态"
+		}
+		else {
+			$pingResultText = "网络未连通，掉线；执行登录: "
+		}
 	}
-
+	
+	# 测试则一定发送登录请求
     if ($Test -or -not $pingResult) {
         if ([bool]$Carrier) {
 			$url = "http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=$($StudentID)@$($Carrier)&user_password=$($Password)"
@@ -58,9 +65,9 @@ function Login-CampusNetwork {
 
         # 弹出系统通知消息
 		# New-BurntToastNotification -Text "已尝试登录校园网", $notification
-		
-		return $notification
     }
+		
+	return $pingResultText + $notification
 }
 
 # 定义注销函数
@@ -69,19 +76,112 @@ function Logout-CampusNetwork {
 	$response = Invoke-WebRequest -Uri $url
 }
 
+# 定义裁剪日志记录函数
+function Cut-FileLog {
+    param (
+        [string[]]$fileContent,
+		[string[]]$s,
+        [int]$num = 100
+    )
+
+	# 分别定义每个字符串包含的行数为 0，分别定义存储每个字符串行数的数组
+	$counts = @{}
+	$lines = @{}
+	foreach ($str in $s) {
+		$counts[$str] = 0
+		$lines[$str] = @()
+	}
+
+	# 循环遍历每一行，记录包含每个字符串的行数并将其存储到相应的数组中
+	foreach ($line in $fileContent) {
+		foreach ($str in $s) {
+			if ($line -match $str) {
+				$counts[$str]++
+				$lines[$str] += $line
+				if ($counts[$str] -gt $num) {
+					$lines[$str] = $lines[$str] | Select-Object -Last $num
+					$counts[$str] = $num
+				}
+				break
+			}
+		}
+	}
+
+	# 按照原先的顺序重新组合文件内容，并返回
+	$newFileContent = @()
+	foreach ($line in $fileContent) {
+		foreach ($str in $s) {
+			if ($line -in $lines[$str]) {
+				$newFileContent += $line
+				break
+			}
+		}
+	}
+	return $newFileContent
+
+}
+
+# 定义取日志文件路径函数
+function Get-FileLog-Path {
+	$logFile = Join-Path $PSScriptRoot "LoginNetwork.log"
+	if (-not (Test-Path $logFile)) {
+		New-Item -Path $logFile -ItemType File -Force
+	}
+	return $logFile
+}
+
+# 定义记录日志函数
+function Write-FileLog {
+    param (
+        [string]$Log,
+		[string]$caller
+    )
+	
+	# 设置日志文件路径
+	$logFile = Get-FileLog-Path
+	
+	# 设置要添加到日志文件中的文本字符串
+	$logFormat = "[{0:yyyy-MM-dd HH:mm:ss}] {1} - {2}"
+	$timestamp = Get-Date
+	$logEntry = $logFormat -f $timestamp, $caller, $Log
+	
+	# 添加新日志内容到日志文件末尾
+	Add-Content $logFile -Value $logEntry
+
+	# 如果日志条数超过 100 条，则只保留最新的 100 条
+	$logContent = Get-Content $logFile
+	Set-Content $logFile (Cut-FileLog $logContent @("Monitor", "Trigger"))
+
+}
+
 # 命令行可接收 5 个参数：学号 密码 运营商 是否测试登录 是否为监测
 # 计划任务执行登录
 if ($args) {
 	
 	# 如果是监测，则7:05 到 23:50 之外的时间以 1 / 20 的概率执行登录
 	$currentDate = Get-Date
-	if ($args[4] -and -not ($currentDate.Hour -gt 7 -or ($currentDate.Hour -ge 7 -and $currentDate.Minute -ge 05)) -and ($currentDate.Hour -lt 23 -or ($currentDate.Hour -eq 23 -and $currentDate.Minute -le 50))) {
-		if (Get-Random -Maximum 20 -Minimum 1 -IncludeMinimum) {
-			Login-CampusNetwork -StudentID $args[0] -Password $args[1] -Carrier $args[2]
+	if ($args[4]) {
+		$caller = "Monitor"
+		$isAfter7_05 = $currentDate.Hour -gt 7 -or ($currentDate.Hour -ge 7 -and $currentDate.Minute -ge 05)
+		$isBefore23_50 = $currentDate.Hour -lt 23 -or ($currentDate.Hour -eq 23 -and $currentDate.Minute -le 50)
+		if (-not ($isAfter7_05 -and $isBefore23_50)){
+			$result = "不在 7:05-23:50 之间, 以 1/20 概率触发登录校园网; "
+			if (Get-Random -Maximum 20 -Minimum 1 -IncludeMinimum) {
+				$result += "触发执行，结果: "
+				$result += Login-CampusNetwork -StudentID $args[0] -Password $args[1] -Carrier $args[2]
+			}
+			else {
+				$result += "未触发执行"
+			}
+		}
+		else {
+			$result = Login-CampusNetwork -StudentID $args[0] -Password $args[1] -Carrier $args[2]
 		}
 	} else {
-		Login-CampusNetwork -StudentID $args[0] -Password $args[1] -Carrier $args[2]
+		$caller = "Trigger"
+		$result = Login-CampusNetwork -StudentID $args[0] -Password $args[1] -Carrier $args[2]
 	}
+	Write-FileLog $result $caller
 	Exit
 }
 
@@ -96,7 +196,7 @@ if (-not $isAdmin) {
 
 # 输出点阵字
 Write-Host @"
-　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
+
 　　　◆◆◆◆◆◆　　◆◆◆◆　◆◆◆◆　◆◆◆◆　　　　◆◆◆　◆◆◆◆◆◆◆◆◆　
 　　◆◆◆　　◆◆　　　◆◆　　　◆◆　　　◆◆◆　　　　◆◆　　◆◆　　◆　　◆◆　
 　　◆◆　　　　◆◆　　◆　　　　　◆　　　　◆◆◆　　◆◆◆　　◆◆　　◆　　◆◆　
@@ -107,18 +207,20 @@ Write-Host @"
 　　◆◆◆　　◆◆◆　　◆◆　　　◆◆　　　　◆　　◆◆　　◆　　　　　　◆　　　　　
 　　　◆◆◆◆◆◆　　　◆◆◆◆◆◆◆　　◆◆◆◆　◆◆◆◆◆◆　　　◆◆◆◆◆　　　
 
-
 "@ -ForegroundColor Blue
 
 # 脚本介绍
-Write-Host "这是一个设置自动登录 CUMT 校园网的程序，支持以下功能：" -ForegroundColor Cyan
-Write-Host "√ 电脑解锁时自动登录" -ForegroundColor Cyan
-Write-Host "√ 连接 CUMT_Stu 或 CUMT_Tec WiFi 时自动登录" -ForegroundColor Cyan
-Write-Host "√ 连接网线（以太网）时自动登录" -ForegroundColor Cyan
-Write-Host "√ 每天 7:22 AM 自动登录" -ForegroundColor Cyan
-Write-Host "√ (可选) 掉线后自动重连（约每 5 分钟检测一次）" -ForegroundColor Cyan
-Write-Host "版本：v20230401" -ForegroundColor Cyan
-Write-Host ""
+Write-Host @"
+这是一个可以设置 CUMT 校园网自动登录的程序，支持以下功能：
+√ 电脑解锁时自动登录
+√ 连接 CUMT_Stu 或 CUMT_Tec WiFi 时自动登录
+√ 连接 CUMT_Stu 网线 (以太网) 时自动登录
+√ 每天 7:22 AM 自动登录
+√ (可选) 掉线时自动重新登录 (自定义循环检测周期)
+多种方式保证您的轻快上网体验！
+版本：v20230402
+
+"@ -ForegroundColor Cyan
 
 <# # 安装 BurntToast
 Write-Host "以下若询问安装模块请输入 y 并回车"
@@ -135,9 +237,15 @@ $taskName2 = "CUMT自动登录校园网——监测"
 $taskExists = Get-ScheduledTask | Where-Object { $_.TaskName -eq $taskName }
 $taskExists2 = Get-ScheduledTask | Where-Object { $_.TaskName -eq $taskName2 }
 if ($taskExists -or $taskExists2) {
-	Write-Host "系统检测到已配置过自动登录。" -ForegroundColor Yellow
+	Write-Host "系统检测到已设置过自动登录。" -ForegroundColor Yellow
+	
+	$logContent = Get-Content (Get-FileLog-Path)
+	Write-Host "最近的执行日志如下："
+	Write-Host ((Cut-FileLog $logContent @('Monitor', 'Trigger') 5) -join "`n")
+	Write-Host ""
+	
 	do {
-		$response = Read-Host "请选择：删除配置 / 覆盖原配置重新配置？(删除 Y / 重新配置 N，默认 重新配置）"
+		$response = Read-Host "请选择：删除设置 / 覆盖原设置重新设置？(删除 Y / 重新设置 N，默认 N）"
 	} while ($response -notmatch "^[ynYN]$" -and $response -ne '')
 
 	if ($taskExists) {
@@ -165,7 +273,39 @@ do {
 	$StudentID = Read-Host "请输入您的学号"
 	$securePassword = Read-Host -Prompt "请输入您的密码" -AsSecureString
 	$Password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword))
-	$Carrier = Read-Host "请输入您的运营商（移动 cmcc，联通 unicom，电信 telecom，校园网 留空）"
+	$Carrier = Read-Host "请输入您的运营商（0.校园网 留空；1.移动 cmcc；2.联通 unicom；3.电信 telecom）"
+	switch ($Carrier) {
+		"0" {
+			$Carrier = ""
+		}
+		"1" {
+			$Carrier = "cmcc"
+		}
+		"2" {
+			$Carrier = "unicom"
+		}
+		"3" {
+			$Carrier = "telecom"
+		}
+		"校园网" {
+			$Carrier = ""
+		}
+		"校园" {
+			$Carrier = ""
+		}
+		"移动" {
+			$Carrier = "cmcc"
+		}
+		"联通" {
+			$Carrier = "unicom"
+		}
+		"电信" {
+			$Carrier = "telecom"
+		}
+		default {
+			Write-Host "未知的运营商" -ForegroundColor Red
+		}
+	}
 
 	do {
 		Write-Host ""
@@ -175,7 +315,7 @@ do {
 		if ($result -eq "您已经处于登录状态") {
 			Write-Host "登录状态下无法验证你的账户信息。" -ForegroundColor Yellow
 			do {
-				$response = Read-Host "是否先注销登录，再重新登录以验证信息是否正确？(Y / N, 默认 Y）"
+				$response = Read-Host "是否先注销登录，再重新登录以验证信息是否正确？(Y / N, 建议验证，默认 Y）"
 			} while ($response -notmatch "^[ynYN]$" -and $response -ne '')
 
 			if ($response -imatch "^[yY]$" -or $response -eq '') {
@@ -204,9 +344,12 @@ Write-Host ""
 
 # 是否掉线自动重登
 do {
-	$response = Read-Host "是否开启掉线自动重连功能，约每 5 分钟检测一次？(Y / N，一般不需要，默认 N)"
-} while ($response -notmatch "^[ynYN]$" -and $response -ne '')
-$reconnect = $response -imatch "^[yY]$"
+	try {
+		[int]$reconnect = Read-Host "是否开启掉线自动重连功能？(若需要请输入检测间隔的分钟数，一般不需要，请直接回车)"
+	} catch {
+		Write-Host "输入无效，请输入一个整数。" -ForegroundColor Red
+	}
+} while (-not $reconnect)
 
 Write-Host "正在设置自动登录..." -ForegroundColor Green
 
@@ -216,7 +359,6 @@ $scriptPath = $MyInvocation.MyCommand.Path
 # 创建计划任务
 $triggerLogin = New-ScheduledTaskTrigger -AtLogOn
 $triggerDaily = New-ScheduledTaskTrigger -At "7:22" -Daily
-$triggerTime = New-ScheduledTaskTrigger -At "0:00" -Once -RepetitionInterval (New-TimeSpan -Minutes 5)
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -ExecutionPolicy Bypass `"$($scriptPath)`" $($StudentID) $($Password) $($Carrier)"
 $action2 = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -ExecutionPolicy Bypass `"$($scriptPath)`" $($StudentID) $($Password) $($Carrier) $false $true"
 $settings = New-ScheduledTaskSettingsSet -DontStopOnIdleEnd -AllowStartIfOnBatteries
@@ -224,6 +366,8 @@ $settings = New-ScheduledTaskSettingsSet -DontStopOnIdleEnd -AllowStartIfOnBatte
 $temp = Register-ScheduledTask -TaskName $taskName -Trigger @($triggerLogin, $triggerDaily) -Action $action -User "SYSTEM" -Settings $settings -RunLevel Highest -Force
 
 if ($reconnect) {
+	Write-Host "正在设置每 $reconnect 分钟循环检测..." -ForegroundColor Green
+	$triggerTime = New-ScheduledTaskTrigger -At "0:00" -Once -RepetitionInterval (New-TimeSpan -Minutes $reconnect)
 	$temp = Register-ScheduledTask -TaskName $taskName2 -Trigger $triggerTime -Action $action2 -User "SYSTEM" -Settings $settings -RunLevel Highest -Force
 }
 
@@ -280,6 +424,7 @@ Add-Network-Event $taskName
 function Add-Random-Delay {
     param (
         [string]$taskName,
+		[string]$delay,
 		[bool]$flag
     )
 
@@ -291,11 +436,11 @@ function Add-Random-Delay {
 	# 随机延时
 	$newRandomDelay = $taskXml.CreateElement("RandomDelay", $taskXml.DocumentElement.NamespaceURI)
 	if ($flag) {
-		$newRandomDelay.InnerText = "PT1M"
+		$newRandomDelay.InnerText = $delay
 		$temp = $taskXml.Task.Triggers.TimeTrigger.AppendChild($newRandomDelay)
 	}
 	else {
-		$newRandomDelay.InnerText = "PT3M"
+		$newRandomDelay.InnerText = $delay
 		$temp = $taskXml.Task.Triggers.CalendarTrigger.AppendChild($newRandomDelay)
 	}
 
@@ -305,22 +450,30 @@ function Add-Random-Delay {
 	
 }
 
-Add-Random-Delay $taskName $false
+Add-Random-Delay $taskName "PT3M" $false
 if ($reconnect) {
-	Add-Random-Delay $taskName2 $true
+	Add-Random-Delay $taskName2 "PT$([Math]::Ceiling($reconnect/3))M" $true
 }
 
 # 输出设置成功信息
-Write-Host ""
-Write-Host "√ 配置完成！若未出现红色错误提示，您的自动登录功能已经生效。" -ForegroundColor Green
-Write-Host "如需修改账户配置，只需重新运行本程序。" -ForegroundColor Green
-Write-Host ""
-Write-Host "本项目链接：https://github.com/zjsxply/CUMT-Network-Login" -ForegroundColor Cyan
-Write-Host "本项目的开发、润色得到了 GPT-4 的协助！如有兴趣，欢迎加入：矿大 ChatGPT 交流群 646745808" -ForegroundColor Cyan
-Write-Host ""
-Write-Host "各学院资源分享群：数学 454162237，化工 808727301（一群）24049485（二群）" -ForegroundColor Cyan
-Write-Host "计算机 916483545，环测 909893238，信控 464112168，机电 717176773，电力 830604599" -ForegroundColor Cyan
-Write-Host ""
+Write-Host @"
+
+√ 设置完成！若未出现红色错误提示，则您的自动登录功能已经生效。
+如需修改设置信息，只需重新运行本程序。
+
+"@ -ForegroundColor Green
+
+# 输出项目信息
+Write-Host @"
+本项目链接：https://github.com/zjsxply/CUMT-Network-Login
+本项目历时约 12 小时上线，期间得到了 GPT-4 的大量帮助！
+如有兴趣，欢迎加入：矿大 ChatGPT 交流群 646745808
+
+各学院资源分享群：数学 454162237，化工 808727301（一群）24049485（二群）
+计算机 916483545，环测 909893238，信控 464112168，机电 717176773，电力 830604599
+
+"@ -ForegroundColor Cyan
+
 $temp = Read-Host "按回车键退出..."
 # Write-Host "按任意键退出..."
 
--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # CUMT-Network-Login
-这是一个设置自动登录 CUMT 校园网的程序，支持以下功能：
+这是一个可以设置 CUMT 校园网自动登录的程序，支持以下功能：
 
-- 电脑解锁后自动登录
-- 连接 CUMT_Stu 或 CUMT_Tec WiFi 后自动登录
-- 连接网线（以太网）后自动登录
+- 电脑解锁时自动登录
+- 连接 `CUMT_Stu` 或 `CUMT_Tec` WiFi 时自动登录
+- 连接 `CUMT_Stu` 网线 (以太网) 时自动登录
 - 每天 7:22 AM 自动登录
-- (可选) 掉线后自动重连（约每 5 分钟检测一次）
+- (可选) 掉线时自动重新登录 (自定义循环检测周期)
+
+多种方式保证您的轻快上网体验！
 
 ## 使用方式
 
 双击运行`CUMT自动登录校园网 for Windows.bat`，需授予管理员权限
+
+## 内容说明
+
+本程序使用 Powershell 编写，主程序在 `LoginNetwork.ps1` 中。
+
+我们将该 Powershell 脚本内嵌在 `CUMT自动登录校园网 for Windows.bat` 中，方便用户使用。
+
+只需下载 `CUMT自动登录校园网 for Windows.bat` 即可。
+
+![screenshot](https://github.com/zjsxply/CUMT-Network-Login/blob/main/screenshot.png?raw=true)
